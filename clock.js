0dd90e75d545e22d2ee59bda06a5b25bad712934
ngoldenberg <Niv.Goldenberg@cushwake.com>
2018-07-23 22:45:38 +0300

    Update cron - 5 sec.

--- clock.js
+++ clock.js
@@ -3,7 +3,7 @@ var CronJob = require('cron').CronJob;
 var worker = require('./worker.js');
 
 var job = new CronJob({
-  cronTime: "05 * * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
+  cronTime: "0-59/5 * * * * *", // everyday, 9:13, 11:13, 4:13, 8:13,
   onTick: worker.start(),
   start: true,
   timeZone: "America/Los_Angeles"
