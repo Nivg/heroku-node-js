35a836304c5f7f758bcabe5c1e90609146be28d6
ngoldenberg <Niv.Goldenberg@cushwake.com>
2018-07-24 11:43:21 +0300

    Add Q

--- clock.js
+++ clock.js
@@ -2,6 +2,22 @@ console.log('IN CLOCK');
 var cron = require('node-cron');
 var worker = require('./worker');
 
+var q = 'tasks';
+
+var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+var open = require('amqplib').connect(url);
+
 cron.schedule('*/2 * * * * *', function () {
-    worker.start();
+    // Publisher
+    open.then(function(conn) {
+        var ok = conn.createChannel();
+        ok = ok.then(function(ch) {
+        ch.assertQueue(q);
+        ch.sendToQueue(q, new Buffer('something to do'));
+        });
+        return ok;
+    }).then(null, console.warn);
 });
+
+
+
