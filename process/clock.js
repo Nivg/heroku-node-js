987695f7c119e1811926cf534f8afbf3cb03baed
ngoldenberg <Niv.Goldenberg@cushwake.com>
2018-07-24 15:08:25 +0300

    Renmae folder

---
+++ process/clock.js
@@ -0,0 +1,24 @@
+console.log('IN CLOCK');
+var cron = require('node-cron');
+
+var q = 'tasks';
+
+var url = process.env.CLOUDAMQP_URL || "amqp://localhost";
+var open = require('amqplib').connect(url);
+
+//cron.schedule('*/15 * * * * *', function () { each 15 seconds
+cron.schedule('* */1 * * *', function () {
+    // Publisher
+    open.then(function(conn) {
+        var ok = conn.createChannel();
+        ok = ok.then(function(ch) {
+        ch.assertQueue(q);
+        console.log('IN CLOCK::sendToQueue::');
+        ch.sendToQueue(q, new Buffer('something to do'));
+        });
+        return ok;
+    }).then(null, console.warn);
+});
+
+
+
